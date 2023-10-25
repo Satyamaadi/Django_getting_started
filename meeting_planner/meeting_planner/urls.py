ac4fc7a99d3487a3ccae8e11d31e5cc77633d838
SHIKHAR <satyamshikhar@gmail.com>
2023-10-25 12:32:16 +0530

    Added function for time

--- meeting_planner/meeting_planner/urls.py
+++ meeting_planner/meeting_planner/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from website.views import welcome, get_time
+from meetings.views import detail
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('welcome.html', welcome),
-    path('time', get_time)
+    path('time', get_time),
+    path('',welcome),
+    path('meeting/<int:id>', detail)
 ]
--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
-
+from meetings.models import Meeting
 
 # Create your views here.
 
 def welcome(request):
-    return HttpResponse('Hello World!')
+    return render(request, 'website/welcome.html',
+                  {"n_users": Meeting.objects.count()})  # multiple vars can be given using commas
 
 
 def get_time(request):
-    return HttpResponse('Time now is: '+ str(datetime.now()))
+    return HttpResponse('Time now is: ' + str(datetime.now()))
